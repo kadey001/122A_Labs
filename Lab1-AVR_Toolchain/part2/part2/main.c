1af2b5e58183196d6e487c48ba8c406fbcf5e3af
Kelton <kadey001@ucr.edu>
2019-10-08 20:21:10 -0700

    Finished fixing and testing lab 1 part 2

--- Lab1-AVR_Toolchain/part2/part2/main.c
+++ Lab1-AVR_Toolchain/part2/part2/main.c
@@ -8,67 +8,89 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include <stdio.h>
+#include <string.h>
 #include "timer.h"
 #include "io.h"
 
 #define uc unsigned char
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
-    DDRA = 0xFF; PORTA = 0x00; //LCD Control Lines
-    DDRB = 0xFF; PORTB = 0x00; //LED output
-    DDRC = 0x00; PORTC = 0xFF; //Button Input
-    DDRD = 0xFF; PORTD = 0x00; //LCD Data Lines
-    /* Insert your solution below */
-    TimerSet(500);
-    TimerOn();
-    
-    uc score = 0x00;
-    uc output = 0x00;
-    uc mainSequence = 0x01;
-    uc flashingLED = 0x00;
-    uc flashingLEDTick = 0x00;
-    uc buttonC0 = 0x00;
+	/* Insert DDR and PORT initializations */
+	DDRA = 0xFF; PORTA = 0x00; //LCD Control Lines
+	DDRB = 0xFF; PORTB = 0x00; //LED output
+	DDRC = 0x00; PORTC = 0xFF; //Button Input
+	DDRD = 0xFF; PORTD = 0x00; //LCD Data Lines
+	/* Insert your solution below */
+	TimerSet(50);
+	TimerOn();
+	
+	LCD_init();
+	LCD_ClearScreen();
+	LCD_DisplayString(1, "0");
+	
+	uc output = 0x00;
+	uc mainSequence = 0x01;
+	uc flashingLED = 0x08;
+	uc flashingLEDTick = 0x00;
+	uc buttonC1 = 0x00;
+	uc pausePressed = 0x00;
+	uc pausedSequence = 0x00;
+	uc paused = 0x00;
+	uc score = 0x00;
 
-    LCD_init();
-    LCD_ClearScreen();
-
-    while (1) {
-        buttonC0 = ~PINC & 0x01;
-        output = mainSequence;
-        switch(mainSequence) {
-            case 0x01: mainSequence = 0x02; break;
-            case 0x02: mainSequence = 0x04; break;
-            case 0x04: mainSequence = 0x01; break;
-            default: mainSequence = 0x01; break;
-        }
-
-        if(buttonC0) {
-            if(output == 0x02) {
-            score++;
-            } else {
-                if(score > 0) {
-                    score--;
-                } else {
-                    //Nothing
-                }
-            }
-        }
-
-        LCD_WriteData(score);
-
-        if(flashingLEDTick == 2) {
-            switch(flashingLED) {
-                case 0x00: flashingLED = 0x08; break;
-                case 0x08: flashingLED = 0x00; break;
-                default: flashingLED = 0x00; break;
-            }
-        }
-        output = output | flashingLED;
-        PORTB = output;
-        flashingLEDTick++;
-        while(!TimerFlag);
-        TimerFlag = 0;
-    }
-    return 1;
+	while (1) {
+		pausePressed = ~PINC & 0x01;
+		buttonC1 = ~PINC & 0x02;
+		if(pausePressed) {
+			paused = !paused;
+		}
+		
+		if(!paused) {
+			if(buttonC1) {
+				if(output == 0x02 || output == 0x0A) {
+					score++;
+					} else {
+					if(score > 0) {
+						score--;
+						} else {
+						//Nothing
+					}
+				}
+				char *scoreString[8];
+				sprintf(scoreString, "%d", score);
+				unsigned char * scoreStringFinal = (unsigned char *)scoreString;
+				LCD_DisplayString(1, scoreStringFinal);
+			}
+			
+			output = mainSequence;
+			switch(mainSequence) {
+				case 0x01: mainSequence = 0x02; break;
+				case 0x02: mainSequence = 0x04; break;
+				case 0x04: mainSequence = 0x01; break;
+				default: mainSequence = 0x01; break;
+			}
+			
+			if(flashingLEDTick == 2) {
+				switch(flashingLED) {
+					case 0x00: flashingLED = 0x08; break;
+					case 0x08: flashingLED = 0x00; break;
+					default: flashingLED = 0x00; break;
+				}
+				flashingLEDTick = 0;
+			}
+			
+			flashingLEDTick++;
+			
+			output |= flashingLED;
+			pausedSequence = output;
+			PORTB = output;
+			} else {
+				PORTB = pausedSequence;
+		}
+		
+		while(!TimerFlag);
+		TimerFlag = 0;
+	}
+	return 1;
 }
--- Lab2-USART/part4/part4/Debug/main.d
+++ Lab2-USART/part4/part4/Debug/main.d
@@ -10,13 +10,8 @@ main.d main.o: .././main.c \
  d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
  d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
  d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
- .././timer.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
- .././io.h
+ .././usart_ATmega1284.h .././timer.h \
+ d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h
 
 d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -40,16 +35,8 @@ d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
 
 d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
-
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
-
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
-
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
+.././usart_ATmega1284.h:
 
 .././timer.h:
 
 d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
-
-.././io.h:
