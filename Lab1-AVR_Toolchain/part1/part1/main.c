ce3f1e8cd73b475a801537e889d4bec27434f929
Kelton <kadey001@ucr.edu>
2019-10-08 19:15:59 -0700

    Fixed Lab1 Part 1 and tested it

--- Lab1-AVR_Toolchain/part1/part1/main.c
+++ Lab1-AVR_Toolchain/part1/part1/main.c
@@ -14,34 +14,53 @@
 
 int main(void) {
 	/* Insert DDR and PORT initializations */
-	DDRB = 0xFF; PORTB = 0x00;
+	DDRB = 0xFF; PORTB = 0x00;//Output
+	DDRC = 0x00; PORTC = 0xFF;//Input
 	/* Insert your solution below */
-	TimerSet(500);
+	TimerSet(50);
+	TimerOn();
 	
 	uc output = 0x00;
 	uc mainSequence = 0x01;
-	uc flashingLED = 0x00;
+	uc flashingLED = 0x08;
 	uc flashingLEDTick = 0x00;
+	uc pausePressed = 0x00;
+	uc pausedSequence = 0x00;
+	uc paused = 0x00;
 
 	while (1) {
-		output = mainSequence;
-		switch(mainSequence) {
-			case 0x01: mainSequence = 0x02; break;
-			case 0x02: mainSequence = 0x04; break;
-			case 0x04: mainSequence = 0x01; break;
-			default: mainSequence = 0x01; break;
+		pausePressed = ~PINC & 0x01;
+		if(pausePressed) {
+			paused = !paused;
 		}
-
-		if(flashingLEDTick == 2) {
-			switch(flashingLED) {
-				case 0x00: flashingLED = 0x08; break;
-				case 0x08: flashingLED = 0x00; break;
-				default: flashingLED = 0x00; break;
+		
+		if(!paused) {
+			output = mainSequence;
+			switch(mainSequence) {
+				case 0x01: mainSequence = 0x02; break;
+				case 0x02: mainSequence = 0x04; break;
+				case 0x04: mainSequence = 0x01; break;
+				default: mainSequence = 0x01; break;
 			}
+		
+			if(flashingLEDTick == 2) {
+				switch(flashingLED) {
+					case 0x00: flashingLED = 0x08; break;
+					case 0x08: flashingLED = 0x00; break;
+					default: flashingLED = 0x00; break;
+				}
+				flashingLEDTick = 0;
+			}
+			
+			flashingLEDTick++;
+			
+			output |= flashingLED;
+			pausedSequence = output;
+			PORTB = output;
+		} else {
+			PORTB = pausedSequence;
 		}
-		output = output | flashingLED;
-		PORTB = output;
-		flashingLEDTick++;
+		
 		while(!TimerFlag);
 		TimerFlag = 0;
 	}
