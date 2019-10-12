b72c22925625af2cb4a81955d0a479a388df84a1
JaeForJett <alee182@ucr.edu>
2019-10-11 19:07:44 -0700

    Create base for L3P1

---
+++ Lab3-SPI/Part1/Part1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Part1.c
+ *
+ * Created: 10/11/2019 7:06:41 PM
+ * Author : Owner
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
