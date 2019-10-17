9fc301f13a0215945999011cf962dd1c1eaccf62
JaeForJett <alee182@ucr.edu>
2019-10-16 17:10:13 -0700

    part 1 and 2 master

--- Lab3-SPI/Part1/Part1/main.c
+++ Lab3-SPI/Part1/Part1/main.c
@@ -8,16 +8,38 @@
 #include <avr/io.h>
 
 
+unsigned char LEDValue;
+
 int main(void)
 {
+	LEDValue = 0;
+	SPI_MasterInit();
+    while (1) 
+    {
+		SPI_MasterTransmit(LEDValue);
+		
+		LEDValue++; //will go back to zero if char overflows
+    }
+}
+
+void SPI_MasterInit(void) {
 	DDRA = 0xFF; PORTA = 0x00;
 	DDRB = 0xBF; PORTB = 0x40;
 	DDRC = 0x00; PORTC = 0xFF;
 	DDRB = 0x00; PORTD = 0xFF;
-    /* Replace with your application code */
-    while (1) 
-    {
-		
-    }
+	
+	SPCR =  (1 << SPE)|(1 << MSTR)|(1 << SPR0);
+	SREG = (1 << 7);
 }
 
+void SPI_MasterTransmit(char cData) {
+	SPDR = cData;
+	
+	PORTB |= 0x10;
+	
+	while(!(SPSR & (1 << SPIF))) {
+		;
+	}
+	
+	PORTB &= 0xEF;
+}
---
+++ Lab3-SPI/Part3/Part3/main.c
@@ -0,0 +1,18 @@
+/*
+ * Part3.c
+ *
+ * Created: 10/16/2019 5:05:53 PM
+ * Author : Owner
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
